80d48c940e44f3983a2ab40aa0b6b362d87da24e
Putra2078 <alphaaldiansyah@gmail.com>
2024-08-20 07:09:47 +0700

    add

---
+++ lib/main.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.lightGreen,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.lightBlue,
+        actions: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(right: 20.0),
+            child: GestureDetector(
+              onTap: () {},
+              child: const Icon(
+                Icons.bug_report,
+                size: 26.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: Center(
+        child: Container(
+          padding: const EdgeInsets.all(20.0),
+          child: Card(
+            child: Container(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                children: <Widget>[
+                  const CircleAvatar(
+                    radius: 50.0,
+                    backgroundImage: NetworkImage(
+                        'https://avatars.githubusercontent.com/u/31731932?v=4'),
+                  ),
+                  const SizedBox(height: 10.0),
+                  const Text(
+                    'M Aldiansyah Putra',
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  const SizedBox(height: 5.0),
+                  const Text(
+                    'XI PPLG-RPL 2',
+                    style: TextStyle(
+                      fontSize: 14.0,
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  const Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(
+                        Icons.school,
+                        size: 30.0,
+                        color: Colors.black,
+                      ),
+                      SizedBox(width: 10.0),
+                      Text(
+                        'Pelajar & game designer',
+                        style: TextStyle(
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20.0),
+                  const Text(
+                    'Saya adalah salah satu murid di kelas XI PPLG-RPL2. Saya juga merupakan seorang penulis kecil, beberapa karya saya yakni novel, puisi, dan game',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 14.0,
+                    ),
+                  ),
+                  const SizedBox(height: 20.0),
+                  ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.grey,
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 50.0, vertical: 15.0),
+                      textStyle: const TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    child: const Text('Follow'),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
